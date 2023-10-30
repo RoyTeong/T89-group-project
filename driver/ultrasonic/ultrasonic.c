43fd799b19c894cefb7d410da05cbe17da2215e5
RoyTeong <85344509+RoyTeong@users.noreply.github.com>
2023-10-30 15:14:57 +0800

    Update ultrasonic.c

--- driver/ultrasonic/ultrasonic.c
+++ driver/ultrasonic/ultrasonic.c
@@ -6,12 +6,15 @@
 #define TRIG_PIN 0
 #define ECHO_PIN 1
 
+// Define a timeout value (in microseconds) for the ultrasonic sensor
 int timeout = 26100;
 
+// Variables to handle the ultrasonic sensor's echo signal
 volatile bool echoReceived = false;
 volatile absolute_time_t pulseStartTime;
 volatile absolute_time_t pulseEndTime;
 
+// Function to set up the GPIO pins for the ultrasonic sensor
 void setupUltrasonicPins(uint trigPin, uint echoPin) {
     gpio_init(trigPin);
     gpio_init(echoPin);
@@ -19,6 +22,7 @@ void setupUltrasonicPins(uint trigPin, uint echoPin) {
     gpio_set_dir(echoPin, GPIO_IN);
 }
 
+// Interrupt service routine for handling the echo signal
 void echo_isr(uint gpio, uint32_t events) {
     if (events & GPIO_IRQ_EDGE_FALL) {
         pulseEndTime = get_absolute_time();
@@ -26,7 +30,9 @@ void echo_isr(uint gpio, uint32_t events) {
     }
 }
 
+// Function to generate a pulse and measure its length
 uint64_t getPulse(uint trigPin, uint echoPin) {
+    // Generate a short pulse on the TRIG pin
     gpio_put(trigPin, 1);
     sleep_us(10);
     gpio_put(trigPin, 0);
@@ -45,17 +51,21 @@ uint64_t getPulse(uint trigPin, uint echoPin) {
     return absolute_time_diff_us(pulseStartTime, pulseEndTime);
 }
 
+// Function to calculate distance in centimeters
 uint64_t getCm(uint trigPin, uint echoPin) {
     uint64_t pulseLength = getPulse(trigPin, echoPin);
-    return pulseLength / 29 / 2; // Convert to centimeters
+    // Convert pulse length to distance in centimeters
+    return pulseLength / 29 / 2;
 }
 
 int main() {
     stdio_init_all();
 
-    uint trigPin = 0; // Replace with your trigger pin number
-    uint echoPin = 1; // Replace with your echo pin number
+    // Define the TRIG and ECHO pins
+    uint trigPin = TRIG_PIN;
+    uint echoPin = ECHO_PIN;
 
+    // Initialize the GPIO pins for the ultrasonic sensor
     setupUltrasonicPins(trigPin, echoPin);
 
     // Initialize pulseStartTime and pulseEndTime to the current time
@@ -63,11 +73,14 @@ int main() {
     pulseEndTime = pulseStartTime;
 
     while (1) {
+        // Get the distance in centimeters
         uint64_t distanceCm = getCm(trigPin, echoPin);
 
+        // Print the measured distance
         printf("Distance: %lld cm ", distanceCm);
 
-        sleep_ms(1000); // Wait for 1 second before the next measurement
+        // Wait for 1 second before the next measurement
+        sleep_ms(1000);
     }
 
     return 0;
