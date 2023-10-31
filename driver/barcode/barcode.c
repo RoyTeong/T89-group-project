19b4cc7ee95846f26ee210efe2c45221c03c72ff
Xue Er <114849603+Xueer2001@users.noreply.github.com>
2023-10-31 13:31:29 +0800

    Update barcode.c with correct variable name and added some comment
    current_barcode_pattern_count_count to current_barcode_pattern_count

--- driver/barcode/barcode.c
+++ driver/barcode/barcode.c
@@ -2,7 +2,6 @@
     Project Scope   : Uses an infrared (IR) sensor to sense the distance between white and black bars on a barcode.
                     : Pico's ADC reads the analog output from the IR sensor connected to GPIO Pin 26
                     : and generates a binary representation of the barcode pattern
-                    : White using the -ve value, Black using the +ve value
     Ouput           : Individual Character - 'W', 'w', 'B', 'b' (refer to Barcode pattern comment)
                     :        Binary String - 0 is black, 1 is white
                     :      Barcode Pattern - 'W' and 'w' represent white, 'B' and 'b' represent black.
@@ -19,14 +18,14 @@
 #include <string.h>       // Standard C library for string manipulation
 
 // Define constant
-#define IR_SENSOR_PIN 26           // GPIO pin connected to the IR sensor
+#define IR_SENSOR_PIN 26            // GPIO pin connected to the IR sensor
 #define ADC_SAMPLE_INTERVAL_MS 100 // Sampling interval for IR sensor (100ms)
 #define MAX_ARRAY_SIZE 100         // Maximum size for the sensor valus array
 
-// Global variable
-int current_barcode_pattern_count_count = 1; // Variable to track the number of each barcode bar type (white or black). -ve for white, +ve for black
-char barcode_letter_data_array[1000]; // Array to store the barcode pattern
-volatile uint16_t ir_sensor_value; // Variable to store the IR sensor value
+// Variables to track and store the letter of the barcode (W,w,B,b)
+char barcode_letter_data_array[1000];         // Array to store the barcode pattern
+int current_barcode_pattern_count = 1; // Variable to track the number of each barcode bar type (white or black). -ve for white, +ve for black
+volatile uint16_t ir_sensor_value;           // Variable to store the IR sensor value
 
 // Define an array to store the sensor values
 uint16_t sensor_values[MAX_ARRAY_SIZE]; // Variable to track the barcode bar type (0 or 1)
@@ -55,11 +54,11 @@ bool ir_sensor_reading_for_barcode (struct repeating_timer *t)
         // Replace the corresponding position in the sensor_values arraywith 1 to represent black
         sensor_values[sensor_values_count++] = 1;
 
-        // Check if the number of 
-        if (current_barcode_pattern_count_count < 0)
+        // Check if the number of c
+        if (current_barcode_pattern_count < 0)
         {
             // If more than 2 consecutive white line (based on sensor_values array), 
-            if (current_barcode_pattern_count_count < -2)
+            if (current_barcode_pattern_count < -2)
             {
                 // When the value is big block white, append W to the barcode_letter_data_array
                 strcat(barcode_letter_data_array, "W");
@@ -142,12 +141,15 @@ bool ir_sensor_reading_for_barcode (struct repeating_timer *t)
 }
 
 int main() {
-    stdio_init_all(); // Initialise standard I/O for console output
-    adc_setup();      // Set up the ADC for IR Sensor readings
+    // Initialise standard I/O for console output
+    stdio_init_all(); 
+    // Set up the ADC for IR Sensor readings
+    adc_setup();      
     
-
     // Set up a timer interrupt for IR sensor sampling every 100 ms
     struct repeating_timer timer;
+
+    // Run the ir_sensor_reading_for_barcode function every 100ms interval
     add_repeating_timer_ms(ADC_SAMPLE_INTERVAL_MS,  ir_sensor_reading_for_barcode, NULL, &timer);
 
     while (true) {
