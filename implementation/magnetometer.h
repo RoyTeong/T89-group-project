68d6bae7db4cef4b630ff68f4c9678ebf25585f2
Phua Tiffany <2202698@sit.singaporetech.edu.sg>
2023-11-29 17:39:58 +0800

    update

--- implementation/magnetometer.h
+++ implementation/magnetometer.h
@@ -31,7 +31,8 @@ float get_heading();
 /**
  * @brief Initialize the I2C bus for communication.
  *
- * This function initializes the I2C bus with a baud rate of 400,000 bps.
+ * This function sets up the I2C bus with a baud rate of 400,000 bps.
+ * Configures the necessary pins and enables pull-ups for communication.
  *
  * @return true if initialization is successful, false otherwise.
  */
@@ -40,51 +41,52 @@ bool initialise_i2c_bus()
     // Initialize the I2C bus
     i2c_init(i2c0, 400000);
    
-    // Make the I2C pins available to picotool
+    // Configure I2C pins for communication
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C); // GPIO 4
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C); // GPIO 5
    
-    // Enable pull up on SDA/SCL lines
+    // Enable pull-up resistors on SDA/SCL lines
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
     return true;
 }
 
 /**
- * @brief Initialize the magnetometer.
+ * @brief Initialize the magnetometer device.
  *
- * This function initializes the magnetometer by configuring its mode and registers.
+ * This function sets up the magnetometer by configuring its mode and registers.
+ * It calls the initialise_i2c_bus() function for I2C setup.
  *
  * @return true if initialization is successful, false otherwise.
  */
 bool initialise_magnetometer()
 {
-    // Initialize I2C bus
+    // Initialize I2C bus for magnetometer
     initialise_i2c_bus();
 
-    // Initialize magnetometer
+    // Configure magnetometer mode and registers
     uint8_t magnetometer_config[] = {MAGNETOMETER_MODE_REGISTER, MAGNETOMETER_CONFIG_REGISTER_A}; // 0x02, 0x00
     i2c_write_blocking(i2c0, MAGNETOMETER_ADDRESS, magnetometer_config, 2, false);
     return true;
 }
 
 /**
- * @brief Get the heading in degrees based on magnetometer data.
+ * @brief Calculates the heading in degrees based on magnetometer data.
  *
- * This function reads the magnetometer data, calculates the heading, and returns the result.
+ * This function reads the magnetometer data, computes the heading angle, and returns the result.
  *
  * @return The heading in degrees.
  */
-float get_heading()
+float get_heading() 
 {
 
     float heading = 0;
 
-    // Write to the magnetometer to tell it to read the data
+    // Request for magnetometer to read the data
     uint8_t magnetometer_reg[1] = {MAGNETOMETER_DATA_REGISTER};
     i2c_write_blocking(i2c0, MAGNETOMETER_ADDRESS, magnetometer_reg, 1, true);
 
-    // Read the data from the magnetometer
+    // Read the data from magnetometer
     uint8_t magnetometer_data[6];
     i2c_read_blocking(i2c0, MAGNETOMETERER_ADDRESS, magnetometer_data, 6, false);
 
@@ -92,10 +94,10 @@ float get_heading()
     int16_t x = (magnetometer_data[0] << 8) | magnetometer_data[1];
     int16_t y = (magnetometer_data[4] << 8) | magnetometer_data[5];
 
-    // Calculate the heading
+    // Calculate the heading using atan2
     heading = atan2(y, x) * 180.0 / M_PI;
 
-    // Normalize to 0-360
+    // Normalize heading to 0-360 degrees
     if (heading < 0)
     {
         heading += 360;
@@ -105,10 +107,10 @@ float get_heading()
 }
 
 /**
- * @brief Get the raw magnetometer data.
+ * @brief Retrieve the raw magnetometer data.
  *
- * This function reads the raw magnetometer data and returns a pointer to an array of three uint16_t values (X, Y, Z).
- * The memory is dynamically allocated, and the caller is responsible for freeing it.
+ * It transforms the data into an array of three uint16_t values (X, Y, Z).
+ * Memory allocation occurs dynamically, and the caller is responsible for releasing it.
  *
  * @return A pointer to an array of three uint16_t values (X, Y, Z).
  */
@@ -117,16 +119,16 @@ uint16_t *get_magnetometer_data()
     uint8_t magnetometer_reg[1] = {MAGNETOMETER_DATA_REGISTER};
     i2c_write_blocking(i2c0, MAGNETOMETER_ADDRESS, magnetometer_reg, 1, true);
 
-    // Read the data from the magnetometer
+    // Retrieve the data from magnetometer
     uint8_t magnetometer_data[6];
     i2c_read_blocking(i2c0, MAGNETOMETER_ADDRESS, magnetometer_data, 6, false);
 
-    // Convert the data to 16-bit signed values
+    // Transform the data into 16-bit signed values
     int16_t x = (magnetometer_data[0] << 8) | magnetometer_data[1];
     int16_t z = (magnetometer_data[2] << 8) | magnetometer_data[3];
     int16_t y = (magnetometer_data[4] << 8) | magnetometer_data[5];
 
-    // put the data into a 16 bit unsigned int
+    // Place the data into a 16-bit unsigned integer
     uint16_t *data = malloc(3 * sizeof(uint16_t));
     data[0] = x;
     data[1] = y;
